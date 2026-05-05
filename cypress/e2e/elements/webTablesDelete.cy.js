69a40f794128a06cc4ffd4eec550301e5b946e8d
Matthew Whitehorn <whitehornmatthew@gmail.com>
2026-05-05 10:05:48 -0400

    Delete entry test

---
+++ cypress/e2e/elements/webTablesDelete.cy.js
@@ -0,0 +1,17 @@
+import { WebTablesPage } from '../../pages/webTables';
+
+const webTablesPage = new WebTablesPage();
+
+describe('WebTables', () => {
+    beforeEach(() => {
+        webTablesPage.navigateToWebTablesPage();
+    });
+
+    it('Web Tables Delete specific entry', () => {
+        cy.fixture('testData').then((testData) => {
+            webTablesPage.webTableBody().should('contain', testData.webTables.deleteEntry.firstName);
+            webTablesPage.deleteButtonByFirstName(testData.webTables.deleteEntry.firstName).click({ force: true });
+            webTablesPage.webTableBody().should('not.contain', testData.webTables.deleteEntry.firstName);
+        });
+    });
+});
--- cypress/pages/webTables.js
+++ cypress/pages/webTables.js
@@ -1,5 +1,13 @@
-export class CheckBoxPage {
-    navigateToCheckBoxPage() {
-        cy.visit('/checkbox');
+export class WebTablesPage {
+    navigateToWebTablesPage() {
+        cy.visit('/webtables');
+    }
+
+    webTableBody() {
+        return cy.get('tbody');
+    }
+
+    deleteButtonByFirstName(firstName) {
+        return cy.get('td').contains(firstName).siblings('td').find('span[title="Delete"]');
     }
 }
